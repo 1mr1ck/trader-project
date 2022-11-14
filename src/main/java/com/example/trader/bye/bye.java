94c936fdfa7f1746aedbfcab1b40efed1b73fcfd
1mr1ck <1mr1ck@naver.com>
2022-11-14 16:38:23 +0900

    #MASTER TEST

--- src/main/java/com/example/trader/bye/bye.java
+++
@@ -1,7 +0,0 @@
-package com.example.trader.bye;
-
-public class bye {
-    public static void main(String[] args) {
-        System.out.println("bye");
-    }
-}
--- src/main/java/com/example/trader/kcontroller/KMainController.java
+++
@@ -1,8 +0,0 @@
-package com.example.trader.kcontroller;
-
-public class KMainController {
-
-    public static void main(String[] args){
-        System.out.println("test");
-    }
-}
