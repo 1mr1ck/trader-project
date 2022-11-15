4723afb317a6fe848f31eacae9b4c1d86af8e2bf
#KST <ymmr8779@naver.com>
2022-11-15 10:03:43 +0900

    커밋테스트2

--- src/main/java/com/example/trader/controller/MainController.java
+++ src/main/java/com/example/trader/controller/MainController.java
@@ -1,4 +1,19 @@
 package com.example.trader.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class MainController {
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping(path = "/join", method = RequestMethod.GET)
+    public String join(){
+        return "join";
+    }
 }
