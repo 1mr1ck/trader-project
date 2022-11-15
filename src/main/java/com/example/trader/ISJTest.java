3691dbc84227f000b5e0f9ec7e06f645d4eecc7f
1mr1ck <1mr1ck@naver.com>
2022-11-15 09:48:45 +0900

    #Master test end project start

--- src/main/java/com/example/trader/ISJTest.java
+++
@@ -1,9 +0,0 @@
-package com.example.trader;
-
-import lombok.Getter;
-
-@Getter
-public class ISJTest {
-    // ISJ Test
-
-}
--- src/main/java/com/example/trader/hello/hello.java
+++
@@ -1,7 +0,0 @@
-package com.example.trader.hello;
-
-public class hello {
-    public static void main(String[] args) {
-        System.out.println("hi");
-    }
-}
