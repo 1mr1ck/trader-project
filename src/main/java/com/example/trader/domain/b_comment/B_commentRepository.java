9c7831fcdca061db8762dbf46d733c950aa9d362
1mr1ck <1mr1ck@naver.com>
2022-11-15 16:08:25 +0900

    # DB연동 해결완료

--- src/main/java/com/example/trader/domain/b_comment/B_commentRepository.java
+++ src/main/java/com/example/trader/domain/b_comment/B_commentRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 public interface B_commentRepository extends JpaRepository<B_comment, Integer> {
 
     @Query(value = "SELECT * FROM b_comment WHERE b_no = ?",nativeQuery = true)
-    List<B_comment> findByB_no(int b_no);
+    public List<B_comment> findByB_no(int b_no);
 }
