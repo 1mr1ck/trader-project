b5a50633ac27ad835c11c0b67bed5dd939e764d8
xortmd <tmd2166@naver.com>
2022-11-17 18:18:10 +0900

    #JTS 18:17

---
+++ src/main/java/com/example/trader/controller/LetterController.java
@@ -0,0 +1,74 @@
+package com.example.trader.controller;
+
+import com.example.trader.domain.letter.Letter;
+import com.example.trader.domain.letter.LetterDto;
+import com.example.trader.domain.product.Product;
+import com.example.trader.service.LetterService;
+import com.example.trader.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class LetterController {
+
+    @Autowired
+    private LetterService service;
+
+    @Autowired
+    private ProductService productService;
+
+
+    // Create
+    @PostMapping("/v1/write/letter")
+    public void writeLetter(@RequestBody LetterDto letterDto) {
+        service.createLetter(letterDto);
+    }
+
+    @GetMapping("/letterWrite/{p_no}/{user_no}")
+    public ModelAndView SearchTotalPageviews(@PathVariable int p_no, @PathVariable int user_no) throws IOException {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("letterWrite");
+        Product product = productService.readProductByP_no(p_no);
+        modelAndView.addObject("productOfLetter", product);
+        List<Letter> letterList = service.readLetterByUser_noAndP_no(user_no, p_no);
+        modelAndView.addObject("myLetterList", letterList);
+        return modelAndView;
+    }
+
+    // GetLetter
+    @GetMapping("/v1/read/letter")
+    public Letter getLetter(@RequestParam int l_code) {
+        Letter letter = service.readLetterByL_code(l_code);
+        return letter;
+    }
+
+    @GetMapping("/v1/read/letter/user_no/{user_no}/p_no/{p_no}")
+    public List<Letter> getLetterByUser_noAndP_no(@PathVariable int user_no, @PathVariable int p_no) {
+        return service.readLetterByUser_noAndP_no(user_no, p_no);
+    }
+
+    // GetLetterAll
+    @GetMapping("/v1/read/letterAll")
+    public List<Letter> getLetterAll() {
+        return service.readLetterAll();
+    }
+
+    // UpdateLetter
+    @PostMapping("/v1/update/letter")
+    public void updateLetter(@RequestBody LetterDto letterDto) {
+        service.updateLetter(letterDto);
+    }
+
+    // DeleteLetter
+    @DeleteMapping("/v1/delete/letter")
+    public void deleteLetter(@RequestParam int l_code) {
+        service.deleteLetter(l_code);
+    }
+
+}
---
+++ src/main/java/com/example/trader/domain/letter/LetterDto.java
@@ -0,0 +1,20 @@
+package com.example.trader.domain.letter;
+
+import com.example.trader.service.util.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class LetterDto extends Timestamp {
+
+    private int l_code;
+    private int user_no;
+    private int p_no;
+    private String nickname;
+    private String l_content;
+}
